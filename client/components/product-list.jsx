2d6ac3d1cbf03439192113bcaca0b9fe7baa6f42
Michaelbaltaza265 <michaelbaltazar265@gmail.com>
2020-11-16 22:59:25 +0000

    mobile

--- client/components/product-list.jsx
+++ client/components/product-list.jsx
@@ -25,11 +25,21 @@ class ProductList extends React.Component {
 
   render() {
     return (
-      this.state.products.map(product => {
-        return (
-          <ProductListItem setView={this.props.setView} key={product.productId} products={product} />
-        );
-      })
+      <main className='row body-section'>
+        <div className='details-container col'>
+          <div className=' d-flex align-items-center justify-content-center '>
+            <div className=' flex-wrap d-flex justify-content-center mb-5'>
+              {
+                this.state.products.map(product => {
+                  return (
+                    <ProductListItem setView={this.props.setView} key={product.productId} products={product} />
+                  );
+                })
+              }
+            </div>
+          </div>
+        </div>
+      </main>
     );
   }
 
