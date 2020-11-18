c1e652db639b931537e1e7245703307a51f0039d
Michaelbaltaza265 <michaelbaltazar265@gmail.com>
2020-11-18 19:05:48 +0000

    making it mobile friendly

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -6,12 +6,12 @@ function CartSummaryItem(items) {
   return (
 
     <>
-      <div className='row product-detail'>
-        <div className='col-5'>
+      <div className='row product-detail body-section p-2'>
+        <div className='col-5 align-items-center p-8'>
           <img className="my-card-img-top img-cart" src={item.image}/>
         </div>
-        <div className=" d-flex flex-column product-info col-5">
-          <h3 className='name'> {item.name} </h3>
+        <div className=" d-flex flex-column product-info col p-4 align-items-center">
+          <h3 className='name p-5'> {item.name} </h3>
           <h4 className='price text-secondary'>{`$${item.price / 100}`}</h4>
           <p className="short-des">{item.shortDescription}</p>
         </div>
